9cfe3c562bd1169f4914a61f16039fd4e3441b6f
masum khan <study.contents.of.masumkhan@gmail.com>
2024-01-02 11:38:55 +0600

    commited

--- src/app/admin/dashboard/content-type-category/page.jsx
+++ src/app/admin/dashboard/content-type-category/page.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const ConTypeCategoryPage = () => {
   return (
     <div>
-      <ConTypeCategory />
+      {/* <ConTypeCategory /> */}
     </div>
   );
 };
--- src/app/admin/dashboard/content-type/page.jsx
+++ src/app/admin/dashboard/content-type/page.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const ContentTypePage = () => {
     return (
         <div>
-            <ContentType/>
+            {/* <ContentType/> */}
         </div>
     );
 };
