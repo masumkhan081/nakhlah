c9e6ac35cc5e024dc3f81458f7d76ffef328e8df
masum khan <study.contents.of.masumkhan@gmail.com>
2024-01-02 14:26:46 +0600

    commited

--- src/components/dashboard-admin/tabular-view/ConTypeCategory.jsx
+++ src/components/dashboard-admin/tabular-view/ConTypeCategory.jsx
@@ -5,46 +5,49 @@ import { useEffect } from "react";
 import { getHandler } from "@/lib/requestHandler";
 import CustomSkeleton from "@/components/ui-custom/CustomSkeleton";
 import ColConTypeCategory from "../table/ColConTypeCategory";
- 
 
 const ConTypeCategory = () => {
-  const conTypeCatagories = useConTypeCategory((state) => state.data);
-  const setConTypeCategories = useConTypeCategory(
-    (state) => state.setConTypeCategories
-  );
-
-  useEffect(() => {
-    const fetch = async () => {
-      const response = await getHandler("content-type-category");
-      console.log(response.data);
-      if (response.status === 200) {
-        const data = response.data.data.map((item) => {
-          return {
-            id: item.id,
-            title: item.attributes.title,
-          };
-        });
-        setConTypeCategories(data);
-      }
-    };
-    if (Array.isArray(conTypeCatagories) && conTypeCatagories.length === 0) {
-      fetch();
-    }
-  }, [conTypeCatagories]);
-
-  return (
-    <div className="w-full bg-white  rounded-xl">
-      {conTypeCatagories.length != 0 ? (
-        <DataTable
-          data={conTypeCatagories}
-          columns={ColConTypeCategory}
-          view={"content-type-category"}
-        />
-      ) : (
-        <CustomSkeleton/>
-      )}
-    </div>
-  );
+  return <>hi hello</>;
 };
+
+// const ConTypeCategory = () => {
+//   const conTypeCatagories = useConTypeCategory((state) => state.data);
+//   const setConTypeCategories = useConTypeCategory(
+//     (state) => state.setConTypeCategories
+//   );
+
+//   useEffect(() => {
+//     const fetch = async () => {
+//       const response = await getHandler("content-type-category");
+//       console.log(response.data);
+//       if (response.status === 200) {
+//         const data = response.data.data.map((item) => {
+//           return {
+//             id: item.id,
+//             title: item.attributes.title,
+//           };
+//         });
+//         setConTypeCategories(data);
+//       }
+//     };
+//     if (Array.isArray(conTypeCatagories) && conTypeCatagories.length === 0) {
+//       fetch();
+//     }
+//   }, [conTypeCatagories]);
+
+//   return (
+//     <div className="w-full bg-white  rounded-xl">
+//       {conTypeCatagories.length != 0 ? (
+//         <DataTable
+//           data={conTypeCatagories}
+//           columns={ColConTypeCategory}
+//           view={"content-type-category"}
+//         />
+//       ) : (
+//         <CustomSkeleton/>
+//       )}
+//     </div>
+//   );
+// };
 
 export default ConTypeCategory;
--- src/components/dashboard-admin/tabular-view/ContentType.jsx
+++ src/components/dashboard-admin/tabular-view/ContentType.jsx
@@ -7,44 +7,47 @@ import { getHandler } from "@/lib/requestHandler";
 import CustomSkeleton from "@/components/ui-custom/CustomSkeleton";
 import ColContentType from "../table/ColContentType";
  
-
 const ContentType = () => {
-  //
-  const conTypeData = useConType((state) => state.data);
-  const setConTypes = useConType((state) => state.setConTypes);
+return <>hi hello</>
+}
 
-  useEffect(() => {
-    const fetch = async () => {
-      const response = await getHandler("content-type");
-      console.log(response.data);
-      if (response.status === 200) {
-        const data = response.data.data.map((item) => {
-          return {
-            id: item.id,
-            title: item.attributes.title,
-          };
-        });
-        setConTypes(data);
-      }
-    };
-    if (Array.isArray(conTypeData) && conTypeData.length === 0) {
-      fetch();
-    }
-  }, [conTypeData]);
+// const ContentType = () => {
+//   //
+//   const conTypeData = useConType((state) => state.data);
+//   const setConTypes = useConType((state) => state.setConTypes);
 
-  return (
-    <div className="w-full bg-white  rounded-xl">
-      {conTypeData.length != 0 ? (
-        <DataTable
-          data={conTypeData}
-          columns={ColContentType}
-          view="content-type"
-        />
-      ) : (
-        <CustomSkeleton/>
-      )}
-    </div>
-  );
-};
+//   useEffect(() => {
+//     const fetch = async () => {
+//       const response = await getHandler("content-type");
+//       console.log(response.data);
+//       if (response.status === 200) {
+//         const data = response.data.data.map((item) => {
+//           return {
+//             id: item.id,
+//             title: item.attributes.title,
+//           };
+//         });
+//         setConTypes(data);
+//       }
+//     };
+//     if (Array.isArray(conTypeData) && conTypeData.length === 0) {
+//       fetch();
+//     }
+//   }, [conTypeData]);
+
+//   return (
+//     <div className="w-full bg-white  rounded-xl">
+//       {conTypeData.length != 0 ? (
+//         <DataTable
+//           data={conTypeData}
+//           columns={ColContentType}
+//           view="content-type"
+//         />
+//       ) : (
+//         <CustomSkeleton/>
+//       )}
+//     </div>
+//   );
+// };
 
 export default ContentType;
