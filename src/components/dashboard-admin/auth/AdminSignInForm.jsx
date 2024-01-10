ab960bd4b0c4ce48b5bf250219945d3ac7f27784
farhan0184 <shahariar0184@gmail.com>
2024-01-10 11:29:39 +0600

    add my file

--- src/components/dashboard-admin/auth/AdminSignInForm.jsx
+++ src/components/dashboard-admin/auth/AdminSignInForm.jsx
@@ -1,15 +1,15 @@
 'use client'
 
-import { Form } from '../ui/form';
+import { Form } from '../../ui/form';
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
-import InputField from '../share/InputField';
-import AdminFormButton from './share/AdminFormButton/AdminFormButton';
 import { useRouter } from 'next/navigation';
 import { toast } from '@/components/ui/use-toast';
 import { useAdminAuth } from '@/store/useAdminStore';
-import { handleSubmit } from '@/components/share/handleSubmit';
+import InputField from '../../ui-custom/InputField';
+import CustomButton from '../../ui-custom/CustomButton';
+import { handleSubmit } from '@/lib/handleSubmit';
 
 const formSchema = z.object({
     email: z.string().min(2, {
@@ -36,19 +36,22 @@ const AdminSignInForm = () => {
     //     form.resetField('password')
     // }
     // submit
-    const onSubmit = async(values) => {
+    const onSubmit = async (values) => {
         const response = await adminAuthCall(values)
-        handleSubmit(response ,toast,'Admin Login',router,'admin/dashboard')
+        handleSubmit(response, toast, 'Admin Login', router, 'admin')
     }
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
 
-                <InputField form={form} name={'email'} placeholder={"Email Address"} type={'email'} isAdmin={true} />
-                <InputField form={form} name={'password'} placeholder={"Password"} type={'password'} isAdmin={true} />
-
-                <AdminFormButton title={'Sign In'} />
+                <InputField form={form} name={'email'} placeholder={"Email Address"} type={'email'} style={'admin-input'} level={'Email'} levelStyle={'text-[16px]'} />
+                <InputField form={form} name={'password'} placeholder={"Password"} type={'password'} style={'admin-input'} level={'Password'} levelStyle={'text-[16px]'}/>
+                <CustomButton
+                    txt={'Sign In'}
+                    type="submit"
+                    style="admin-btn"
+                />
             </form>
         </Form>
     );
